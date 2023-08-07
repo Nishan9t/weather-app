9f3e7c03112d6391e4acec2c4896ae545decd028
Nishan9t <pandeynisha9t@gmail.com>
2023-08-07 16:54:45 +0530

    API is connected and data is fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Search from './Components/Search';
 import Result from './Components/Result';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [search,setSearch] = useState("");
+  const [weather,setWeather] = useState([]);
+
+  const changeSearch=(value)=>{
+    setSearch(value);
+  }
+
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  const searchWeatherHandler=()=>{
+    if(search !=="")
+    {
+      axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=936c3efbb52773e196ed8950141edcdd`
+      )
+      .then(
+        (response)=>{
+          console.log(response)
+        }
+      ).catch(
+        (error)=>{
+          console.log(error)
+        }
+      )
+    }
+   
+
+  }
+
+  // useEffect(()=>{
+  //   if(search !=="")
+  //   {
+  //     getWeatherData();
+  //   }
+  // },[search])
+
+  const searchWeather=()=>{
+
+  }
+
   return (
     <div className='max-w-[1240px] mx-auto mt-2 p-3'>
-      <Search/>
-      <Result/>
+      <Search searchData={search} eventHandle={changeSearch} searchWeather={searchWeatherHandler}/>
+      <Result weatherData={weather}/>
     </div>
 
   );
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export default function Search() {
+export default function Search(props) {
+
+  const searchInput = useRef();
+
   return (
     <div className='flex shadow-xl'>
-    <input type="search" className='border border-black w-full p-3 text-2xl'/>
-    <button className='p-3 bg-slate-600 text-white'>Search</button>
+    <input type="search" value={props.searchData} onChange={()=>props.eventHandle(searchInput.current.value)} className='border border-black w-full p-3 text-2xl'ref={searchInput}/>
+    <button onClick={props.searchWeather} className='p-3 bg-slate-600 text-white'>Search</button>
     </div>
   )
 }
