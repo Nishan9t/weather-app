abfe2190b604ae20d2ee1e7a7899c3177a20ed6f
Nishan9t <pandeynisha9t@gmail.com>
2023-08-07 11:19:55 +0530

    search UI is completed

---
+++ src/Components/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div className='flex'>
+    <input type="search" className='border border-black w-full p-3 text-2xl'/>
+    <button className='p-3 bg-slate-600 text-white'>Search</button>
+    </div>
+  )
+}
